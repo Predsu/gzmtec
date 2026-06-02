aa2049934ebc68b9297f134cd3fd2235a9e90a74
ToriYukari <kuklaradek1@gmail.com>
2026-06-02 16:33:15 +0200

    added weekday to scripts and db

--- backend/models/deviationLogger.model.js
+++ backend/models/deviationLogger.model.js
@@ -1,8 +1,16 @@
 const db = require('../config/db');
 
+const WEEKDAY_TIME_ZONE = process.env.WEEKDAY_TIME_ZONE || 'Europe/Warsaw';
+
+const getCurrentWeekdayName = () => new Intl.DateTimeFormat('en-US', {
+    weekday: 'long',
+    timeZone: WEEKDAY_TIME_ZONE,
+}).format(new Date());
+
 const deviationLoggerModel = {
     logDeviation:  async (trip, lineLabel, stop_id, vehicle_id, deviation) => {
-        const result = await db.query("INSERT INTO deviations (trip, lineLabel, stop_id, vehicle_id, deviation, arrival_time) VALUES (?, ?, ?, ?, ?, NOW())", [trip, lineLabel, stop_id, vehicle_id, deviation]);
+        const weekday = getCurrentWeekdayName();
+        const result = await db.query("INSERT INTO deviations (trip, lineLabel, stop_id, vehicle_id, deviation, weekday, arrival_time) VALUES (?, ?, ?, ?, ?, ?, NOW())", [trip, lineLabel, stop_id, vehicle_id, deviation, weekday]);
         return result[0].insertId;
     },
     displayDeviationsAverage: async () => {
